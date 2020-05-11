2061f6d7aa117eca6f754f92992c6794f1356553
simplyWiri <48577820+simplyWiri@users.noreply.github.com>
2020-05-11 19:00:36 +1000

    Updated submodules to 1.1

---
+++ 1.1/CombatExtended/Harmony/Harmony_ExplosionCE.cs
@@ -0,0 +1,26 @@
+﻿using CombatExtended;
+using FrontierDevelopments.Shields.Harmony;
+using HarmonyLib;
+using System.Collections.Generic;
+using Verse;
+
+namespace FrontierDevelopments.CombatExtendedIntegration.Harmony
+{
+    public class Harmony_ExplosionCE : Harmony_Explosion
+    {
+        private static int GetDamage(ExplosionCE explosion, IntVec3 cell)
+        {
+            return explosion.GetDamageAmountAtCE(cell);
+        }
+
+        [HarmonyPatch(typeof(ExplosionCE), nameof(ExplosionCE.Tick))]
+        private static class Patch_Tick
+        {
+            [HarmonyPrefix]
+            private static void HandleOuterEdgesFirst(ExplosionCE __instance, int ___startTick, List<IntVec3> ___cellsToAffect)
+            {
+                HandleProtected(___cellsToAffect, __instance, ___startTick, GetDamage);
+            }
+        }
+    }
+}
---
+++ 1.1/CombatExtended/Harmony/Module.cs
@@ -0,0 +1,21 @@
+﻿using CombatExtended;
+using FrontierDevelopments.Shields.Harmony;
+using HarmonyLib;
+using RimWorld;
+
+namespace FrontierDevelopments.CombatExtendedIntegration.Harmony
+{
+    public class Module
+    {
+        [HarmonyPatch(typeof(DefGenerator), nameof(DefGenerator.GenerateImpliedDefs_PostResolve))]
+        private static class Load
+        {
+            [HarmonyPostfix]
+            private static void Postfix()
+            {
+                Harmony_Verb.BlacklistType(typeof(Verb_ShootMortarCE));
+                Harmony_Verb.BlacklistType(typeof(Verb_MarkForArtillery));
+            }
+        }
+    }
+}
